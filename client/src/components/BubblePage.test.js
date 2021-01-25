da5602165c91b71ac6152f4807f01909cb7d6939
Xaver Pinero <xaver.pinero@gmail.com>
2021-01-24 20:37:53 -0500

    test

--- client/src/components/BubblePage.test.js
+++ client/src/components/BubblePage.test.js
@@ -1,7 +1,43 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import BubblePage from "./BubblePage";
 
 test("Fetches data and renders the bubbles", () => {
   // Finish this test
+  render(<BubblePage />);
+
+  const getColorsList = require('./BubblePage');
+
+  jest.mock('./BubblePage');
+  async () => {
+    axios.get({
+      data: [
+        {
+          color: "aliceblue",
+          code: {
+            hex: "#f0f8ff",
+          },
+          id: 1,
+        },
+        {
+          color: "limegreen",
+          code: {
+            hex: "#99ddbc",
+          },
+          id: 2,
+        },
+        {
+          color: "aqua",
+          code: {
+            hex: "#00ffff",
+          },
+          id: 3,
+        },
+      ],
+    });
+
+    const colorList = await getColorsList();
+    expect(colorList).toHaveLength(3);
+  };
 });
+
